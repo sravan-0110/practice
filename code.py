2c7b407c7905a0e417ee1ad85dc9001a23351301
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-18 00:09:56 +0530

    ifstatment

--- code.py
+++ code.py
@@ -7,3 +7,6 @@ print("Guess a number between 1 and 10")
 while True:
     guess = int(input("Your guess: "))
     attempts += 1
+    if guess == number:
+        print(f"Correct! You guessed it in {attempts} attempts.")
+        break
